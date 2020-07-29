cc3da28a73c52c9823e956cabce06fe32267cefe
ryo.arakawa <arkw.bump@gmail.com>
2020-07-29 17:15:10 +0000

    07/30

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,57 @@
+class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    counts(@user)
+    @likes = current_user.favposts.order(id: :desc).page(params[:page])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user= User.new(user_params)
+    
+    if @user.save
+      flash[:success] ="ユーザーを登録しました。"
+      redirect_to @user
+    else
+      flash.now[:danger] ="ユーザーの登録に失敗しました。"
+      render :new
+    end  
+  end
+  
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+  
+  def likes
+    @user = User.find(params[:id])
+    @likes = @user.likes.page(params[:page])
+    counts(@user)
+
+  end  
+  
+
+ 
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation)
+end
